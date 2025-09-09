ec15a6f73dcb6fa20b561f4544c0164b237f515e
SilvoidVorstellung <Silvoid@protonmail.com>
2025-09-10 06:43:02 +0800

    Use nmcli instead of ping to check for connection. modified:   turnWifiOn.sh

--- turnWifiOn.sh
+++ turnWifiOn.sh
@@ -2,10 +2,14 @@
 
 # nmcli radio wifi on
 
-if ping -c 1 archlinux.org &>/dev/null; then
+# if ping -c 1 archlinux.org &>/dev/null; then
+#   nmcli radio wifi off
+# else
+#   nmcli radio wifi on
+# fi
+
+if nmcli -t -f active,ssid, dev wifi | grep -q '^yes'; then
   nmcli radio wifi off
-  echo "wifi turn off"
 else
   nmcli radio wifi on
-  echo "wifi turn on"
 fi
