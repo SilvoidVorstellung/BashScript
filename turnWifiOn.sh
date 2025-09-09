b59d1c38ba8d852f2c9a2c4f076a89eaebe79ea9
SilvoidVorstellung <Silvoid@protonmail.com>
2025-09-10 05:20:35 +0800

    Made it toggle-able instead of just turning on wifi. modified:   turnWifiOn.sh

--- turnWifiOn.sh
+++ turnWifiOn.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-nmcli radio wifi on
+# nmcli radio wifi on
+
+if ping -c 1 archlinux.org &>/dev/null; then
+  nmcli radio wifi off
+  echo "wifi turn off"
+else
+  nmcli radio wifi on
+  echo "wifi turn on"
+fi
